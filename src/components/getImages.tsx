b19df094ec67fe38777744c45c0df6efa6fbdedb
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2024-02-26 08:53:16 +0100

    update the finalPrompt

--- src/components/getImages.tsx
+++ src/components/getImages.tsx
@@ -1,20 +1,21 @@
 const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
 
 const getImages = async (prompt: string) => {
-  const finalPrompt = `
-  "Cher modèle AI,
-  Je suis à la recherche d'images mangas dans un style particulier pour mes projets créatifs.
-  Je préfère des détails nets et des couleurs mangas pour que l'image soit convaincante.
+//   const finalPrompt = `
+//   "Cher modèle AI,
+//   Je suis à la recherche d'images mangas dans un style particulier pour mes projets créatifs.
+//   Je préfère des détails nets et des couleurs mangas pour que l'image soit convaincante.
   
-  Voici quelques points clés à considérer :
+//   Voici quelques points clés à considérer :
   
-      Sujet : ${prompt}
-      Style : realiste
-      Atmosphère : joyeuse
-      Détails : ${prompt}
-  Merci beaucoup pour votre aide, j'ai hâte de voir ce que vous allez créer!"
-  `;
-
+//       Sujet : ${prompt}
+//       Style : realiste
+//       Atmosphère : joyeuse
+//       Détails : ${prompt}
+//   Merci beaucoup pour votre aide, j'ai hâte de voir ce que vous allez créer!"
+//   `;
+  const finalPrompt = prompt;
+  
   const options = {
     method: "POST",
     headers: {
